33052282632d2468ee3a00866927ac8ec4c3f84b
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2024-05-03 17:38:21 +1000

    Initial commit

--- src/main/java/com/sonphattran/resumeportal/ResumePortalApplication.java
+++ src/main/java/com/sonphattran/resumeportal/ResumePortalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResumePortalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResumePortalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResumePortalApplication.class, args);
+    }
 
 }
