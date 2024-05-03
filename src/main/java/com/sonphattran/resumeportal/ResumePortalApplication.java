4a0012f5245e68f8205f208169ff82977f91d788
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2024-05-03 17:38:02 +1000

    Initial commit

---
+++ src/main/java/com/sonphattran/resumeportal/ResumePortalApplication.java
@@ -0,0 +1,13 @@
+package com.sonphattran.resumeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonphattran/resumeportal/ResumePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonphattran.resumeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
