747f7f1cd0dfbd3cdb79de389f853247af3d6795
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2024-05-03 19:37:17 +1000

    Add path-based security

---
+++ src/main/java/com/sonphattran/resumeportal/configuration/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.sonphattran.resumeportal.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth ->
+                    auth.requestMatchers("/home*").authenticated()
+                            .requestMatchers("/**").permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
