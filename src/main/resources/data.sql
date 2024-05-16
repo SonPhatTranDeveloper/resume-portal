6af7cfcc66b9da8c215046304cd6bc2d87f1fe2a
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2024-05-16 17:52:32 +1000

    Remove test data initialization.

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-/* Insert skill */
-INSERT INTO
-    my_user (id, username, password, roles, active, visible, updated)
-VALUES
-    (100, 'user1', 'password1', 'USER', FALSE, FALSE, TRUE),
-    (200, 'user2', 'password2', 'USER', FALSE, FALSE, TRUE);
